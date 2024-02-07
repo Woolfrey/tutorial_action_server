cf90529d20e7a736d353357aede97dba7b7d009a
Jon <jonathan.woolfrey@gmail.com>
2024-02-07 15:12:04 +0000

    Saving work in progress

--- src/fibonacci_server.cpp
+++ src/fibonacci_server.cpp
@@ -9,7 +9,7 @@
 #include "rclcpp_action/rclcpp_action.hpp"
 #include "rclcpp_components/register_node_macro.hpp"
 
-class FibonacciServer : rclcpp::Node
+class FibonacciActionServer : public rclcpp::Node
 {
      public:
           using Fibonacci  = tutorial_action_definition::action::Fibonacci;                         // Makes referencing easier
@@ -19,122 +19,59 @@ class FibonacciServer : rclcpp::Node
           /**
            * Constructor.
            */
-          FibonacciServer(const rclcpp::NodeOptions & options = rclcpp::NodeOptions())
-          : Node("action_server_node", options)
+          FibonacciActionServer(const rclcpp::NodeOptions & options = rclcpp::NodeOptions())
+          : Node("fibonacci_action_server_node", options)
           {
-          
+               using namespace std::placeholders;                                                   // Needed for _1, _2 arguments below
+               
+               this->_actionServer = rclcpp_action::create_server<Fibonacci>(this,
+                                                                             "fibonacci",
+                                                                             std::bind(&FibonacciActionServer::request_action, this, _1, _2),
+                                                                             std::bind(&FibonacciActionServer::cancel_action,  this, _1),
+                                                                             std::bind(&FibonacciActionServer::accept_request, this, _1));
           }
      
      private:
 
-//          rclcpp_action::Server<Fibonacci> _actionServer;                                           // ROS2 server with Fibonacci as the action type
+          rclcpp_action::Server<Fibonacci>::SharedPtr _actionServer;                                // ROS2 server with Fibonacci as the action type
 
-          /*
-           * Function for requesting Fibonacci calculation.
-           * @param goal An integer specifying which number in the sequence.
-           *
-          rclcpp_action::GoalResponse handle_goal(const rclcpp_action::GoalUUID &uuid,
-                                                  const Fibonacci::Goal &goal)
+          rclcpp_action::GoalResponse request_action(const rclcpp_action::GoalUUID          &uuid,
+                                                     std::shared_ptr<const Fibonacci::Goal> goal)
           {
-               RCLCPP_INFO(this->get_logger(), "Requested to compute Fibonacci sequence up to order %d", goal->order); // Inform user
-
-               (void)uuid;                                                                          // I don't know what this does ¯\_(ツ)_/¯
-
-               return rclcpp::ACCEPT_AND_EXECUTE;
+               return rclcpp_action::GoalResponse::ACCEPT_AND_EXECUTE;
           }
-
-           *
-           * Function for cancelling an action under execution.
-           * @goalHandle
-           *
-          rclcpp_action::CancelResponse handle_cancel(const GoalHandle &goalHandle)
+          
+          rclcpp_action::CancelResponse cancel_action(const std::shared_ptr<GoalHandle> goal_handle)
           {
-               RCLCPP_INFO(this->get_logger(), "Received request to cancel goal.");                 // Inform user
-
-               (void)goal_handle;                                                                   // I don't know what this does ¯\_(ツ)_/¯
-
                return rclcpp_action::CancelResponse::ACCEPT;
           }
-
           
-           * Function for when a goal is accepted for execution.
-           *
-           *
-          void handle_accepted(const GoalHandle &goalHandle)
+          void accept_request(const std::shared_ptr<GoalHandle> goal_handle)
           {
-               std::thread{std::bind(&FibonacciServer::execute, this, _1), goalHandle}.detach();    // Link to execute() function below
+               // Worker bees can leave.
+               // Even drones can fly away.
+               // The Queen is their slave.
           }
-
           
-           * Principle control thread which computes the Fibonacci sequence.
-           *
-          void execute(const GoalHandle &goalHandle)
+          void execute(const std::shared_ptr<GoalHandle> goal_handle)
           {
-               RCLCPP_INFO(this->get_logger(), "Executing goal.");                                  // Inform user
-
-               rclcpp::Rate loopRate(1);                                                            // Set a frequency of 1Hz for this thread
-
-               int goal = goalHandle->get_goal();                                                   // Obtain the goal
-
-               auto feedback = Fibonacci::Feedback;
-
-               int sequence[] = feedback.partial_sequence;
-
-               sequence.push_back(0);
-
-               sequence.push_back(1);
-
-               auto result = Fibonacci::Result;
-
-               for(int i = 1; i < goal.order) && rclcpp::ok(); i++)
-               {
-                    // Check for cancellation
-                    if(goalHandle.is_cancelling())
-                    {
-                         result.sequence = sequence;                                                // Put the current sequence as the result
-
-                         goalHandle.canceled(result);                                               // Add to goal handle
-
-                         RCLCPP_INFO(this->get_logger(), "Goal cancelled.");                        // Inform user
-
-                         return;                                                                    // Exit the thread
-                    }
-
-                    sequence.push_back(sequence[i] + sequence[i-1]);                                // Compute next number in sequence
-   
-                    goalHandle.publish_feedback(feedback);                                          // As it says
-
-                    RCLCPP_INFO(this->get_logger(), "Publishing feedback.");                        // Inform user
-
-                    loopRate.sleep();                                                               // Synchronize with the control frequency
-               }
-
-               // Check if the objective has been achieved
-               if(rclcpp::ok())
-               {
-                    result.sequence = sequence;
-
-                    goalHandle.succeed(result);
-
-                    RCLCPP_INFO(this->get_logger(), "Goal succeeded.");
-               }
-          }*/
+          
+          }
 };                                                                                                  // Semicolon required after class definition
 
 
-////////////////////////////////////////////////////////////////////////////////////////////////////
-//                                           MAIN                                                 //
+  ///////////////////////////////////////////////////////////////////////////////////////////////////
+ //                                           MAIN                                                //
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 int main(int argc, char **argv)
 {
-/*
-     rclcpp::init(argc, argv);
+     rclcpp::init(argc, argv);                                                                      // Start up ROS2
+     
+     auto actionServerNode = std::make_shared<FibonacciActionServer>();                             // Create node (as shared pointer)
 
-     auto action_server = std::make_shared<ActionServer>();
+     rclcpp::spin(actionServerNode);
 
-     rclcpp::spin(action_server);
-
-     rclcpp::shutdown();*/
+     rclcpp::shutdown();
      
      return 0;
 }
